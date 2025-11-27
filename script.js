eefbbaa2c999e40be79754ee0eaa02eafae7d2cc
unknown <rahatbhai180@gmail.com>
2025-11-27 15:00:14 +0600

    update js file

--- script.js
+++ script.js
@@ -23,6 +23,48 @@ const products = {
         price:"890 Tk",
         models:"iPhone 11-12 Pro Max 13-13 Pro -13 Pro Max 14-14 Pro -14 Pro Max 15-15 Pro -15 Pro Max 16-16 Pro -16 Pro Max Also 15 Plus /14 Plus",
         images:["images/product4-1.jpg","images/product4-2.jpg","images/product4-3.jpg","images/product4-4.jpg"]
+    },
+
+    "LuxuryCase5":{
+        name:"Luxury Case 5",
+        price:"820 Tk",
+        models:"Compatible with iPhone 14, 14 Pro, 14 Pro Max",
+        images:["images/product5-1.jpg","images/product5-2.jpg","images/product5-3.jpg","images/product5-4.jpg"]
+    },
+
+    "LuxuryCase6":{
+        name:"Luxury Case 6",
+        price:"780 Tk",
+        models:"Compatible with iPhone 13, 13 Pro, 13 Pro Max",
+        images:["images/product6-1.jpg","images/product6-2.jpg","images/product6-3.jpg","images/product6-4.jpg"]
+    },
+
+    "LuxuryCase7":{
+        name:"Luxury Case 7",
+        price:"750 Tk",
+        models:"Compatible with iPhone 12, 12 Pro, 12 Pro Max",
+        images:["images/product7-1.jpg","images/product7-2.jpg","images/product7-3.jpg","images/product7-4.jpg"]
+    },
+
+    "LuxuryCase8":{
+        name:"Luxury Case 8",
+        price:"770 Tk",
+        models:"Compatible with iPhone 11, 11 Pro, 11 Pro Max",
+        images:["images/product8-1.jpg","images/product8-2.jpg","images/product8-3.jpg","images/product8-4.jpg"]
+    },
+
+    "LuxuryCase9":{
+        name:"Luxury Case 9",
+        price:"740 Tk",
+        models:"Compatible with iPhone X, XR, XS, XS Max",
+        images:["images/product9-1.jpg","images/product9-2.jpg","images/product9-3.jpg","images/product9-4.jpg"]
+    },
+
+    "LuxuryCase10":{
+        name:"Luxury Case 10",
+        price:"720 Tk",
+        models:"Compatible with iPhone 8, 8 Plus",
+        images:["images/product10-1.jpg","images/product10-2.jpg","images/product10-3.jpg","images/product10-4.jpg"]
     }
 };
 
@@ -73,11 +115,12 @@ $(document).ready(function(){
             slider.data('owl.carousel').to(number,300,true);
         });
 
-        // Quantity buttons
+        // Qty Buttons
         $(".qtyminus").click(function(){
             var now=$(".qty").val();
             if($.isNumeric(now)&&parseInt(now)-1>0) $(".qty").val(parseInt(now)-1);
         });
+
         $(".qtyplus").click(function(){
             var now=$(".qty").val();
             if($.isNumeric(now)) $(".qty").val(parseInt(now)+1);
